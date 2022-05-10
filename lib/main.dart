8ccb12dc7bd2f7e9f1e730356dbfc933dd296b12
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-05-11 00:57:35 +0100

    commit main

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,21 @@ import 'package:flash_chat/screens/chat_screen.dart';
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
-      home: WelcomeScreen(),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            theme: ThemeData.dark().copyWith(
+                textTheme: TextTheme(
+                    bodyText1: TextStyle(color: Colors.black54),
+                ),
+            ),
+            routes: {
+                WelcomeScreen.id: (context) => WelcomeScreen(),
+                ChatScreen.id: (context) => ChatScreen(),
+                LoginScreen.id: (context) => LoginScreen(),
+                RegistrationScreen.id: (context) => RegistrationScreen(),
+            },
+            initialRoute: WelcomeScreen.id,
+        );
+    }
 }
